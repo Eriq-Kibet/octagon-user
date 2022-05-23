872190f67536c0b1a03735cbcdfd05b44d17cb62
Eric <ericthedev@gmail.com>
2022-05-23 07:40:25 -0400

    feat(client) login UI, register UI

---
+++ client/src/views/LoginView.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <h1>Octagon User Login</h1>
+    <br />
+    <br />
+    <div class="row">
+      <div class="col-md-3"></div>
+      <div class="col-md-6">
+        <form>
+          <div class="form-group">
+            <input
+              type="number"
+              class="form-control"
+              id="phoneNumber"
+              placeholder="Enter phonenumber"
+            />
+            <small id="phoneHelp" class="form-text text-muted"
+              >We'll never share your phone number with anyone else.</small
+            >
+          </div>
+          <div class="form-group">
+            <input
+              type="password"
+              class="form-control"
+              id="exampleInputPassword1"
+              placeholder="Password"
+            />
+          </div>
+
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+      </div>
+      <div class="col-md-3"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
---
+++ client/src/views/RegisterView.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container">
+    <h1>Octagon User Registration</h1>
+    <br />
+    <div class="row">
+      <div class="col-md-3"></div>
+      <div class="col-md-6">
+        <form>
+          <div class="form-group">
+            <input
+              type="text"
+              class="form-control"
+              id="firstname"
+              placeholder="Enter First Name"
+            />
+          </div>
+          <div class="form-group">
+            <input
+              type="text"
+              class="form-control"
+              id="lastname"
+              placeholder="Enter Last Name"
+            />
+          </div>
+          <div class="form-group">
+            <input
+              type="number"
+              class="form-control"
+              id="phoneNumber"
+              placeholder="Enter phonenumber"
+            />
+          </div>
+          <div class="form-group">
+            <input
+              type="password"
+              class="form-control"
+              id="exampleInputPassword1"
+              placeholder="Password"
+            />
+          </div>
+
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+      </div>
+      <div class="col-md-3"></div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+
+<style scoped></style>
