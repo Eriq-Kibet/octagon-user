3a063d39bb87b712399bbde861e20146bc26be55
Eric <ericthedev@gmail.com>
2022-05-23 09:41:54 -0400

    feat(axios) data fetchin from backend

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -6,37 +6,65 @@
     <div class="row">
       <div class="col-md-3"></div>
       <div class="col-md-6">
-        <form>
-          <div class="form-group">
-            <input
-              type="number"
-              class="form-control"
-              id="phoneNumber"
-              placeholder="Enter phonenumber"
-            />
-            <small id="phoneHelp" class="form-text text-muted"
-              >We'll never share your phone number with anyone else.</small
-            >
-          </div>
-          <div class="form-group">
-            <input
-              type="password"
-              class="form-control"
-              id="exampleInputPassword1"
-              placeholder="Password"
-            />
-          </div>
+        <div class="form-group">
+          <input
+            type="number"
+            class="form-control"
+            id="phoneNumber"
+            v-model="phonenumber"
+            placeholder="Enter phonenumber"
+          />
+          <small id="phoneHelp" class="form-text text-muted"
+            >We'll never share your phone number with anyone else.</small
+          >
+        </div>
+        <div class="form-group">
+          <input
+            type="password"
+            class="form-control"
+            id="password"
+            v-model="password"
+            placeholder="Password"
+          />
+        </div>
 
-          <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
+        <button @click="login" class="btn btn-primary">Submit</button>
       </div>
       <div class="col-md-3"></div>
     </div>
+    {{ info }}
   </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+export default {
+  name: "loginView",
+  data() {
+    return {
+      phonenumber: "",
+      password: "",
+    };
+  },
+  methods: {
+    login: function () {
+      console.log("phone" + this.phonenumber);
+      console.log("pass" + this.password);
+    },
+  },
+  mounted() {
+    axios
+      .get("http://localhost:8888/api/users", {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods":
+            "GET, POST, PATCH, PUT, DELETE, OPTIONS",
+          "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token",
+        },
+      })
+      .then((response) => (this.info = response));
+  },
+};
 </script>
 
 <style scoped></style>
--- client/src/views/RegisterView.vue
+++ client/src/views/RegisterView.vue
@@ -5,49 +5,69 @@
     <div class="row">
       <div class="col-md-3"></div>
       <div class="col-md-6">
-        <form>
-          <div class="form-group">
-            <input
-              type="text"
-              class="form-control"
-              id="firstname"
-              placeholder="Enter First Name"
-            />
-          </div>
-          <div class="form-group">
-            <input
-              type="text"
-              class="form-control"
-              id="lastname"
-              placeholder="Enter Last Name"
-            />
-          </div>
-          <div class="form-group">
-            <input
-              type="number"
-              class="form-control"
-              id="phoneNumber"
-              placeholder="Enter phonenumber"
-            />
-          </div>
-          <div class="form-group">
-            <input
-              type="password"
-              class="form-control"
-              id="exampleInputPassword1"
-              placeholder="Password"
-            />
-          </div>
+        <div class="form-group">
+          <input
+            type="text"
+            class="form-control"
+            id="firstname"
+            v-model="firstname"
+            placeholder="Enter First Name"
+          />
+        </div>
+        <div class="form-group">
+          <input
+            type="text"
+            class="form-control"
+            id="lastname"
+            v-model="lastname"
+            placeholder="Enter Last Name"
+          />
+        </div>
+        <div class="form-group">
+          <input
+            type="number"
+            class="form-control"
+            id="phoneNumber"
+            v-model="phonenumber"
+            placeholder="Enter phonenumber"
+          />
+        </div>
+        <div class="form-group">
+          <input
+            type="password"
+            class="form-control"
+            id="password"
+            v-model="password"
+            placeholder="Password"
+          />
+        </div>
 
-          <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
+        <button @click="register" class="btn btn-primary">Submit</button>
       </div>
       <div class="col-md-3"></div>
     </div>
   </div>
 </template>
 <script>
-export default {};
+export default {
+  name: "registerView",
+  data() {
+    return {
+      firstname: "",
+      lastname: "",
+      phonenumber: "",
+      password: "",
+    };
+  },
+  methods: {
+    register: function () {
+      console.log("first" + this.firstname);
+      console.log("last" + this.lastname);
+      console.log("phone" + this.phonenumber);
+      console.log("pass" + this.password);
+    },
+  },
+};
 </script>
 
 <style scoped></style>
